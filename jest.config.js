0a9bc7908299b865727808f89492fd19d3a8e539
Andrew Smith <andrewsmith@alumni.stanford.edu>
2023-10-01 13:59:38 -0400

    Repro.

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const config = {
+  "projects": ["src/project1", "src/project2"],
+  "testTimeout": 30000,
+  "setupFilesAfterEnv": [
+    "jest-extended/all"
+  ],
+  "verbose": true
+}
+  
+module.exports = config
